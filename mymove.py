01bdce8fe8c0a77a0e9e5ca2bb5ade0efc3b486b
선우 김 <rlatjsdn104201@naver.com>
2021-08-02 22:55:47 +0900

    commit

---
+++ mymove.py
@@ -0,0 +1,20 @@
+﻿import tkinter.messagebox as msgbox
+from tkinter import *
+
+def targetHit():
+    root = Tk()
+    root.withdraw()
+    msgbox.showinfo("성공", "배를 성공적으로 맞췄습니다!")
+def targetMiss():
+    root = Tk()
+    root.withdraw()
+    msgbox.showerror("실패", "배를 맞추지 못했습니다!")
+
+def targetClear():
+    root = Tk()
+    root.withdraw()
+    msgbox.showinfo("격추", "배를 성공적으로 격추시켰습니다!")
+targetHit()
+targetMiss()
+targetClear()
+
