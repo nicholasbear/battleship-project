7f62c3db058e30260886a980c14adfec2ce04416
nicholasbear <50478347+nicholasbear@users.noreply.github.com>
2021-08-04 20:05:44 +0900

    commit

--- phase2.py
+++ phase2.py
@@ -1,9 +1,10 @@
 import ai
 import enemyboard
 import random
-from tkinter import*
+import tkinter
+from tkinter import *
 
-window=Tk()
+window=tkinter.Tk()
 window.title("Battleship by 머저리")
 window.geometry("1200x640+100+100")
 window.resizable(False, False)
@@ -29,8 +30,10 @@ mshowboard= [[0 for col in range(10)] for row in range(10)]                     
 #display 되는 부분 //////////////////////////////////////////////////////////////////////////////////////////
 me = Label(window,text='Me',width = 4, height = 2, fg="black")                        #위에 타이틀
 me.place(x=220,y=0,width=40,height=40)
+log = Label(window,text='log',width = 4, height = 2, fg="black")                        #위에 타이틀
+log.place(x=580,y=0,width=40,height=40)
 com = Label(window,text='Com',width = 4, height = 2, fg="black")
-com.place(x=740,y=0,width=40,height=40)
+com.place(x=940,y=0,width=40,height=40)
 ##########################################
 for i in range(10):                                                                     # 보드 표시부분
     for j in range(10):
@@ -94,7 +97,7 @@ temp.place(x=1120,y=480)
 shotboat4 = Label(window,text='?',width = 4, height = 2)
 shotboat4.place(x=1120,y=520)
 
-
+'''
 #############################################################
 #행열 입력하는부분
 temp = Label(window, text = "행 : ",width = 4, height = 2)
@@ -109,14 +112,18 @@ columninput.place(x = 660, y = 480)
 
 firebutton = Button(window, text = "가즈아~", bg = "alice blue", command = ###############)
 firebutton.place(x = 560, y = 520)
-
+'''
 ###################################################################
 #로그부분
 
-
-
-
-
+logbox=Listbox(window,width=34,height=22)
+logbox.place(x=480,y=40)
+scrollbar=Scrollbar(window,orient="vertical")
+scrollbar.config(command=logbox.yview)
+scrollbar.pack(side="right",fill="y")
+logbox.config(yscrollcommand=scrollbar.set)
+for x in range(1000):
+    logbox.insert(END, str(x))
 
 
 
