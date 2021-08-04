0f02d1f1b765e8cee7e1a1746254fd1bb472d6b0
nicholasbear <50478347+nicholasbear@users.noreply.github.com>
2021-08-04 18:57:48 +0900

    commit

--- phase2.py
+++ phase2.py
@@ -5,7 +5,7 @@ from tkinter import*
 
 window=Tk()
 window.title("Battleship by 머저리")
-window.geometry("1000x640+100+100")
+window.geometry("1200x640+100+100")
 window.resizable(False, False)
 '''
 mb=Label(window, text="myboard", bg="yellow", fg="black")
@@ -54,23 +54,72 @@ for i in range(10):
     for j in range(10):
         if eshowboard[i][j]==0:
             test = Label(window,width = 4, height = 2, relief="solid", bg = "lavender")
-            test.place(x=40*(i+14),y=40*j+40,width=40,height=40)
+            test.place(x=40*(i+19),y=40*j+40,width=40,height=40)
         elif eshowboard[i][j]==1:
             test = Label(window,width = 4, height = 2, relief="solid", bg = "black")
-            test.place(x=40*(i+14),y=40*j+40,width=40,height=40)
+            test.place(x=40*(i+19),y=40*j+40,width=40,height=40)
         elif eshowboard[i][j]==2:
             test = Label(window,text='2',width = 4, height = 2, relief="solid", bg = "yellow")
-            test.place(x=40*(i+14),y=40*j+40,width=40,height=40)
+            test.place(x=40*(i+19),y=40*j+40,width=40,height=40)
         elif eshowboard[i][j]==3:
             test = Label(window,text='3',width = 4, height = 2, relief="solid", bg = "red")
-            test.place(x=40*(i+14),y=40*j+40,width=40,height=40)
+            test.place(x=40*(i+19),y=40*j+40,width=40,height=40)
         elif eshowboard[i][j]==4:
             test = Label(window,text='4',width = 4, height = 2, relief="solid", bg = "blue")
-            test.place(x=40*(i+14),y=40*j+40,width=40,height=40)
+            test.place(x=40*(i+19),y=40*j+40,width=40,height=40)
 ###########################################################
 #격추됬는지 표시부분
-temp = Label(window,test='2번배',width = 4, height = 2, relief="solid", bg = "lavender")
-       temp.place(x=40*(i+14),y=40*j+40,width=40,height=40)
+temp = Label(window,text='2번배',width = 4, height = 2)
+temp.place(x=40,y=480)
+shotboat2 = Label(window,text='?',width = 4, height = 2)
+shotboat2.place(x=40,y=520)
+temp = Label(window,text='3번배',width = 4, height = 2)
+temp.place(x=200,y=480)
+shotboat3 = Label(window,text='?',width = 4, height = 2)
+shotboat3.place(x=200,y=520)
+temp = Label(window,text='4번배',width = 4, height = 2)
+temp.place(x=360,y=480)
+shotboat4 = Label(window,text='?',width = 4, height = 2)
+shotboat4.place(x=360,y=520)
+temp = Label(window,text='2번배',width = 4, height = 2)
+temp.place(x=800,y=480)
+shotboat2 = Label(window,text='?',width = 4, height = 2)
+shotboat2.place(x=800,y=520)
+temp = Label(window,text='3번배',width = 4, height = 2)
+temp.place(x=960,y=480)
+shotboat3 = Label(window,text='?',width = 4, height = 2)
+shotboat3.place(x=960,y=520)
+temp = Label(window,text='4번배',width = 4, height = 2)
+temp.place(x=1120,y=480)
+shotboat4 = Label(window,text='?',width = 4, height = 2)
+shotboat4.place(x=1120,y=520)
+
+
+#############################################################
+#행열 입력하는부분
+temp = Label(window, text = "행 : ",width = 4, height = 2)
+temp.place(x = 500, y = 480)
+rowinput = Entry(window, width = 4)
+rowinput.place(x = 540, y = 480)
+
+temp = Label(window, text = "열 : ",width = 4, height = 2)
+temp.place(x = 620, y = 480)
+columninput = Entry(window, width = 4)
+columninput.place(x = 660, y = 480)
+
+firebutton = Button(window, text = "가즈아~", bg = "alice blue", command = ###############)
+firebutton.place(x = 560, y = 520)
+
+###################################################################
+#로그부분
+
+
+
+
+
+
+
+
 
 
 
